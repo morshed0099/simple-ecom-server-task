562f3e786c87c6a17051712a016327ee392d7da4
morshed0099 <mdgolammorshed0099@gmail.com>
2023-04-13 21:24:30 +0600

    mongodb databse add

--- index.js
+++ index.js
@@ -1,27 +1,38 @@
-const express=require('express');
-const app =express();
-const port=process.env.PORT || 5000;
-const cors=require('cors');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors');
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 //midleware 
 app.use(cors());
 app.use(express.json())
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1m4kiwj.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const productsCollection = client.db('simpleEcom').collection('products')
+const categoryCollection = client.db('simpleEcom').collection('category')
 
-async function run(){
-    try{
+async function run() {
+    try {
 
-    }finally{
+        app.get('/products', async (req, res) => {
+            const query = {}
+            const products = await productsCollection.find(query).toArray()
+            res.send(products)
+        })
+
+    } finally {
 
     }
 
-}run().catch(error=>console.error(error));
+} run().catch(error => console.error(error));
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('server is running')
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`server is running on ${port}`);
 })
