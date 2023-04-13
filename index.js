c0f9c9c0b6cf4363714f51fb1c6f59d2da375471
morshed0099 <mdgolammorshed0099@gmail.com>
2023-04-13 21:34:36 +0600

    product view with id

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 //midleware 
 app.use(cors());
@@ -21,6 +21,12 @@ async function run() {
             const query = {}
             const products = await productsCollection.find(query).toArray()
             res.send(products)
+        })
+        app.get('/productdetails/:id',async(req,res)=>{
+            const id = req.params.id
+            const query={_id:new ObjectId(id)}
+            const result=await productsCollection.findOne(query);
+            res.send(result);
         })
 
     } finally {
